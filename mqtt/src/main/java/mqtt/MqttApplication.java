ed6c63e6d2d375a5af78899fd0b4f3ea1a5347b8
paulscott56 <pscott209@gmail.com>
2016-04-18 14:22:21 +0200

    added MQTT spring integration. Needs better Bean definitions, no XML

--- mqtt/src/main/java/mqtt/MqttApplication.java
+++ mqtt/src/main/java/mqtt/MqttApplication.java
@@ -1,12 +1,56 @@
 package mqtt;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.env.Environment;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Template;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
-@SpringBootApplication
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Configuration
+@ImportResource("/integration.xml")
 public class MqttApplication {
 
+    private String consumerKey = "so5cCgtLyhRoTCBWHA3eZ99xR";
+
+    private String consumerSecret = "Q1TbhbxF7eC4CXIJ8OJbt629U7DzFYB9pQB7wdWO1z3xbX9bqi";
+
     public static void main(String[] args) {
-        SpringApplication.run(MqttApplication.class, args);
+        new AnnotationConfigApplicationContext(MqttApplication.class);
     }
+
+    @Bean
+    public String newline() {
+        return System.getProperty("line.separator");
+    }
+
+    @Bean
+    public String tstamp() {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        return formatter.format(new Date());
+    }
+
+    @Bean
+    public Twitter twitterTemplate() {
+        System.out.println(consumerKey);
+        return new TwitterTemplate(consumerKey, consumerSecret);
+    }
+
+    @Bean
+    public DefaultMqttPahoClientFactory clientFactory() {
+        // could define username and password for broker connection here
+        return new DefaultMqttPahoClientFactory();
+    }
+
+
 }
