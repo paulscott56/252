23c9ce5115b1ebbf83f29b2f6174317c96c21975
Paul Scott <pscott209@gmail.com>
2016-04-24 16:47:04 +0200

    added java config for mqtt inbound and outbound flows

--- mqtt/src/main/java/mqtt/MqttApplication.java
+++ mqtt/src/main/java/mqtt/MqttApplication.java
@@ -1,56 +1,100 @@
 package mqtt;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.log4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.core.env.Environment;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.endpoint.MessageProducerSupport;
+import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
-import org.springframework.social.oauth2.OAuth2Operations;
-import org.springframework.social.oauth2.OAuth2Template;
-import org.springframework.social.twitter.api.Twitter;
-import org.springframework.social.twitter.api.impl.TwitterTemplate;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.integration.stream.CharacterStreamReadingMessageSource;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-@Configuration
-@ImportResource("/integration.xml")
+@IntegrationComponentScan
+@SpringBootApplication
 public class MqttApplication {
 
-    private String consumerKey = "so5cCgtLyhRoTCBWHA3eZ99xR";
+    private static final Logger LOGGER = Logger.getLogger(MqttApplication.class);
 
-    private String consumerSecret = "Q1TbhbxF7eC4CXIJ8OJbt629U7DzFYB9pQB7wdWO1z3xbX9bqi";
-
-    public static void main(String[] args) {
-        new AnnotationConfigApplicationContext(MqttApplication.class);
+    /**
+     * Load the Spring Integration Application Context
+     *
+     * @param args - command line arguments
+     */
+    public static void main(final String... args) {
+        SpringApplication.run(MqttApplication.class, args);
     }
 
     @Bean
-    public String newline() {
-        return System.getProperty("line.separator");
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        factory.setServerURIs("tcp://192.168.0.3:1883");
+        return factory;
+    }
+
+    // publisher
+
+    @Bean
+    public IntegrationFlow mqttOutFlow() {
+        return IntegrationFlows.from(CharacterStreamReadingMessageSource.stdin(),
+                e -> e.poller(Pollers.fixedDelay(1000)))
+                .transform(p -> p)
+                .handle(mqttOutbound())
+                .get();
     }
 
     @Bean
-    public String tstamp() {
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
-        return formatter.format(new Date());
+    public MessageHandler mqttOutbound() {
+        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler("Publisher", mqttClientFactory());
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultTopic("/outTopic");
+        return messageHandler;
+    }
+
+    // consumer
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
     }
 
     @Bean
-    public Twitter twitterTemplate() {
-        System.out.println(consumerKey);
-        return new TwitterTemplate(consumerKey, consumerSecret);
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter("tcp://192.168.0.3:1883", "testClient",
+                        "/inTopic");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
     }
 
     @Bean
-    public DefaultMqttPahoClientFactory clientFactory() {
-        // could define username and password for broker connection here
-        return new DefaultMqttPahoClientFactory();
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println(message.getPayload());
+            }
+
+        };
     }
-
-
 }
